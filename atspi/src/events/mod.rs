1160179ec69fc94b4feee32bc2e7ebce7bf557f1
Tait Hoyem <tait@tait.tech>
2023-05-04 16:10:27 -0600

    GenericEvent::build() now returns a result

--- atspi/src/events/mod.rs
+++ atspi/src/events/mod.rs
@@ -28,6 +28,7 @@ pub const CACHE_ADD_SIGNATURE: Signature<'_> =
 	Signature::from_static_str_unchecked("((so)(so)(so)iiassusau)");
 
 use std::collections::HashMap;
+use std::ops::Deref;
 
 use serde::{Deserialize, Serialize};
 use zbus::{
@@ -162,8 +163,8 @@ impl GenericEvent<'_> for AddAccessibleEvent {
 
 	type Body = CacheItem;
 
-	fn build(item: Accessible, body: Self::Body) -> Self {
-		Self { item, node_added: body }
+	fn build(item: Accessible, body: Self::Body) -> Result<Self, AtspiError> {
+		Ok(Self { item, node_added: body })
 	}
 
 	fn sender(&self) -> UniqueName<'_> {
@@ -199,8 +200,8 @@ impl GenericEvent<'_> for RemoveAccessibleEvent {
 
 	type Body = Accessible;
 
-	fn build(item: Accessible, body: Self::Body) -> Self {
-		Self { item, node_removed: body }
+	fn build(item: Accessible, body: Self::Body) -> Result<Self, AtspiError> {
+		Ok(Self { item, node_removed: body })
 	}
 	fn sender(&self) -> UniqueName<'_> {
 		self.item.name.clone().into()
@@ -225,6 +226,32 @@ impl_to_dbus_message!(RemoveAccessibleEvent);
 pub struct Accessible {
 	pub name: OwnedUniqueName,
 	pub path: OwnedObjectPath,
+}
+impl TryFrom<zvariant::OwnedValue> for Accessible {
+	type Error = zbus::Error;
+	fn try_from(value: zvariant::OwnedValue) -> Result<Self, Self::Error> {
+		match value.deref() {
+			zvariant::Value::Structure(s) => {
+				if s.signature() != ACCESSIBLE_PAIR_SIGNATURE {
+					return Err(zbus::Error::Variant(zvariant::Error::SignatureMismatch(s.signature(), format!("To turn a zvariant::Value into an atspi::Accessible, it must be of type {}", ACCESSIBLE_PAIR_SIGNATURE.as_str()))));
+				}
+				let fields = s.fields();
+				let name_value: String = fields.get(0).ok_or(zbus::Error::Variant(zvariant::Error::IncorrectType))?.try_into()?;
+				let path_value: String = fields.get(1).ok_or(zbus::Error::Variant(zvariant::Error::IncorrectType))?.try_into()?;
+				let name = UniqueName::try_from(name_value)?.into();
+				let path = ObjectPath::try_from(path_value)?.into();
+				Ok(Accessible {
+					name, path
+				})
+			},
+			_ => Err(zbus::Error::Variant(zvariant::Error::IncorrectType)),
+		}
+	}
+}
+impl From<Accessible> for zvariant::Structure<'_> {
+	fn from(accessible: Accessible) -> Self {
+		(accessible.name.as_str().to_string(), accessible.path).into()
+	}
 }
 impl Default for Accessible {
 	fn default() -> Self {
@@ -317,8 +344,8 @@ impl GenericEvent<'_> for EventListenerDeregisteredEvent {
 
 	type Body = EventListeners;
 
-	fn build(item: Accessible, body: Self::Body) -> Self {
-		Self { item, deregistered_event: body }
+	fn build(item: Accessible, body: Self::Body) -> Result<Self, AtspiError> {
+		Ok(Self { item, deregistered_event: body })
 	}
 	fn sender(&self) -> UniqueName<'_> {
 		self.item.name.clone().into()
@@ -348,8 +375,8 @@ impl GenericEvent<'_> for EventListenerRegisteredEvent {
 
 	type Body = EventListeners;
 
-	fn build(item: Accessible, body: Self::Body) -> Self {
-		Self { item, registered_event: body }
+	fn build(item: Accessible, body: Self::Body) -> Result<Self, AtspiError> {
+		Ok(Self { item, registered_event: body })
 	}
 	fn sender(&self) -> UniqueName<'_> {
 		self.item.name.clone().into()
@@ -379,8 +406,8 @@ impl GenericEvent<'_> for AvailableEvent {
 
 	type Body = Accessible;
 
-	fn build(item: Accessible, body: Self::Body) -> Self {
-		Self { item, socket: body }
+	fn build(item: Accessible, body: Self::Body) -> Result<Self, AtspiError> {
+		Ok(Self { item, socket: body })
 	}
 	fn sender(&self) -> UniqueName<'_> {
 		self.item.name.clone().into()
@@ -472,7 +499,7 @@ pub trait GenericEvent<'a> {
 	type Body: Type + Serialize + Deserialize<'a>;
 
 	/// Build the event from the object pair (Accessible and the Body).
-	fn build(item: Accessible, body: Self::Body) -> Self;
+	fn build(item: Accessible, body: Self::Body) -> Result<Self, AtspiError> where Self: Sized;
 
 	/// Path of the signalling object.
 	fn path(&self) -> ObjectPath<'_>;
